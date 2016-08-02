5a578a3694d08d0defe3aac6dc171c2eeb32e906
asutedja <asutedja@gmail.com>
2016-08-01 20:34:08 -0700

    added blue nodes

--- src/testDancer.js
+++ src/testDancer.js
@@ -1,8 +1,18 @@
-var testDancer = function(top, left, timeBetweenSteps) {
+var makeTestDancer = function(top, left, timeBetweenSteps) {
+  //var obj = Object.create(prototype);
   makeDancer.call(this, top, left, timeBetweenSteps);
-
+  
+  this.$node = $('<span class="testdancer"></span>');
+  this.setPosition(top, left);
 };
 
-testDancer.prototype = Object.create(makeDancer.prototype);
-testDancer.prototype.constructor = testDancer;
 
+makeTestDancer.prototype = Object.create(makeDancer.prototype);
+makeTestDancer.prototype.constructor = makeTestDancer;
+
+
+makeTestDancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+  this.$node.toggle();
+
+};
